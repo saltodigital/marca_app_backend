fc8612d4e06941df87f8c73545dc85523a085cdb
Janneison Galindo <janneisongalindo@gmail.com>
2017-12-11 11:48:55 -0500

    Login realizado y terminado

--- marcaAPP/marcaAPP/urls.py
+++ marcaAPP/marcaAPP/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'^rest-auth/', include('rest_auth.urls'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('rest-auth/', include('rest_auth.urls'))
 ]
