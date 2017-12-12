ccb77bc78ddc33addc7e31bc529ddb8de6dd4665
Janneison Galindo <janneisongalindo@gmail.com>
2017-12-12 15:47:25 -0500

    agrego modelos

--- marcaAPP/parametrizacion/models.py
+++ marcaAPP/parametrizacion/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 #Modelos generales.
 class BaseModel(models.Model):
@@ -29,7 +30,7 @@ class Persona(BaseModel):
 		(u'2',u'Femenino'),)
     estadoCiviles = ((u'0',u'[Seleccione...]'),(u'1',u'Soltero'),
 		(u'2',u'Casado'),)	
-	rut = models.IntegerField()
+	rut = models.CharField(max_length=50, unique=True)
 	#tipoIdentificacion = models.ForeignKey(Tipo , related_name = 'fk_Persona_tipo',on_delete=models.PROTECT)
 	primerApellido = models.CharField(max_length=100)
     segundoApellido = models.CharField(max_length=100)
@@ -39,22 +40,28 @@ class Persona(BaseModel):
     correoElectronico = models.EmailField(max_length=200)
     telefono = models.CharField(max_length=100)
     telefonoFijo = models.CharField(max_length=100)
-    
+    municipio = models.ForeignKey(Municipio)
+
 	def __str__(self):
 		return self.nombre + ' ' + self.primerApellido
 
-	class Meta:		
-		unique = [
-			["rut",],
-		]
-
 class Empresa(BaseModel):
-	rut = models.CharField(max_length=255, unique=True)
+	rut = models.CharField(max_length=50, unique=True)
+    field = models.CharField(max_length=100, unique=True)
+    rut = models.CharField(max_length=255, unique=True)
 	direccion = models.CharField(max_length=255)
 
 class Cargo(BaseModel):
 	empresa = models.ForeignKey(Empresa)
 	firma_cartas = models.BooleanField(default=False)
+
+class User(AbstractUser):
+    persona = models.ForeignKey(Persona)
+    cargo = models.ForeignKey(Cargo)
+
+    class Meta:
+        db_table = 'auth_user'
+
 
 
 
