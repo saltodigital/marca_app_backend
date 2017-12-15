430ee26432db6c7a72defcaee8ea3b3ea0c4f192
Janneison Galindo <janneisongalindo@gmail.com>
2017-12-15 16:05:40 -0500

    Update settings.py

--- marcaAPP/settings.py
+++ marcaAPP/settings.py
@@ -189,7 +189,7 @@ SWAGGER_SETTINGS = {
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
