9bb594f09184f4e9b267f1d02df36318aaaa6a1f
Janneison Galindo <janneisongalindo@gmail.com>
2018-02-16 09:55:53 -0500

    datetime

--- asistencia/views.py
+++ asistencia/views.py
@@ -20,7 +20,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view
-
+import datetime
 
 class AsistenciaViewSet(viewsets.ModelViewSet):
     """
